ec0f3144e0b5792d8d1c8c44341c4499fa8840c2
hhosaka <hhosaka183@gmail.com>
2015-04-05 22:54:27 +0900

    Datadeleter

--- app/src/main/java/com/nag/android/bs_match_maker/DataDeleter.java
+++ app/src/main/java/com/nag/android/bs_match_maker/DataDeleter.java
@@ -1,9 +1,12 @@
 package com.nag.android.bs_match_maker;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
+import android.view.View;
+import android.widget.Button;
 
 public class DataDeleter {
 	public static void show(final Context context, String title){
@@ -12,7 +15,12 @@ public class DataDeleter {
 		new AlertDialog.Builder(context)
 		.setIcon(android.R.drawable.ic_dialog_info)
 		.setTitle(title)
-		.setMultiChoiceItems(files, flags, null)
+		.setMultiChoiceItems(files, flags, new DialogInterface.OnMultiChoiceClickListener(){
+            @Override
+            public void onClick(DialogInterface dialogInterface, int index, boolean flag) {
+                flags[index]=flag;
+            }
+        })
  		.setNegativeButton("Cancel", null)
         .setPositiveButton("OK", new OnClickListener() {
             public void onClick(DialogInterface dialog, int whichButton) {
